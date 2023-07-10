8f7de83c5bb9c3ae1b33045a15f1850f26605df0
Sahil <sahil310g@gmail.com>
2023-07-10 22:23:12 +0530

    Resolved issues

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import useFetch from '../../hooks/useFetch'
-import DetailsBanner from './detailsBanner/DetailsBanner'
+// import DetailsBanner from './detailsBanner/DetailsBanner'
 
 const Details = () => {
   // const {media_type, id} = useParams();
   // const {data, loading} = useFetch(`/${media_type}/${id}`)
   return (
     <div>
-      <DetailsBanner/>
+      {/* <DetailsBanner/> */}
     </div>
   )
 }
